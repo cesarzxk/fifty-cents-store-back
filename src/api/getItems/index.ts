c620e50877ada28bc572978058a044d1e5f882ba
cesarzxk <43748428+cesarzxk@users.noreply.github.com>
2022-03-24 08:24:03 -0300

    Adicionado filtros.
    Foi adicionado possibilidades de filtros de categoria ou/e materiais ou palavras via atributos opcionais fornecidas via parâmetros  'category', 'material'  e  'search'.
    
    Algumas prevenções de erros foram aplicadas.

--- src/api/getItems/index.ts
+++ src/api/getItems/index.ts
@@ -2,8 +2,36 @@ import http from 'http';
 import {Response} from 'express';
 import dataFormatter from '../../services/dataFormatter';
 import options from '../options/'
+import dataFilter from '../../services/dataFilter';
+import dataSearch from '../../services/dataSearch';
 
-export async function getItems(res:Response, locale:string , id?:string){
+type filtersType={
+    category:string[],
+    material:string[]
+}
+
+
+type productType = {
+    hasDiscount: boolean,
+    name: string,
+    images: string[],
+    description: string,
+    price: string,
+    discountValue:string,
+    material: string,
+    category: string,
+    id: string,
+    locale:string
+}
+
+export default async function getItems(
+    res:Response, 
+    locale:string, 
+    material?:string[],
+    category?:string[],
+    id?:string,
+    search?:string,
+    ){
     http.get(options(locale, id), (result)=>{
         var data = "";
         result.on("data",(chunk)=>{
@@ -11,8 +39,27 @@ export async function getItems(res:Response, locale:string , id?:string){
         })
 
         result.on('end',()=>{
-            const newData = dataFormatter(JSON.parse(data), locale)
-            res.status(200).json(newData).end()
+            const newData = dataFormatter(JSON.parse(data), locale) as productType[]
+            
+            if(material||category)
+            {
+                return res.status(200).json(dataFilter(newData, material, category, search)).end();
+
+            }else{
+                if (search){
+                    const dataSearched = dataSearch(newData, search) as productType[]|[]
+                    
+                    if(dataSearched.length === 0){
+                        console.log(dataSearched)
+                        return res.status(401).end();
+                        
+                    }
+                    return res.status(200).json(dataSearched).end()
+
+                }else{
+                    return res.status(200).json(newData).end();
+                }
+            }
         })
     });
     
