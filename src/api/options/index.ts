763bd242b77732a2c52be78bc2cd095eac66b6b2
cesarzxk <cesar3d.vargas@gmail.com>
2023-04-22 18:30:56 -0300

    feat: change api option url

--- src/api/options/index.ts
+++ src/api/options/index.ts
@@ -1,13 +1,13 @@
 function options(locale: string, id?: string) {
   if (id) {
     return {
-      host: "616d6bdb6dacbb001794ca17.mockapi.io",
-      path: `/devnology/${locale}_provider/${id}`,
+      host: "https://json-mock-api.herokuapp.com/",
+      path: `/${locale}_provider/${id}`,
     };
   } else {
     return {
-      host: "616d6bdb6dacbb001794ca17.mockapi.io",
-      path: `/devnology/${locale}_provider`,
+      host: "https://json-mock-api.herokuapp.com/",
+      path: `/${locale}_provider`,
     };
   }
 }
