151161387161eb0ea22558c8cde149cb3508a13d
cesarzxk <cesar3d.vargas@gmail.com>
2023-05-06 17:05:11 -0300

    change engine

--- build/src/api/options/index.js
+++ build/src/api/options/index.js
@@ -3,14 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 function options(locale, id) {
     if (id) {
         return {
-            host: "json-mock-api.herokuapp.com",
-            path: "/".concat(locale, "_provider/").concat(id),
+            host: "616d6bdb6dacbb001794ca17.mockapi.io",
+            path: "/devnology/".concat(locale, "_provider/").concat(id),
         };
     }
     else {
         return {
-            host: "json-mock-api.herokuapp.com",
-            path: "/".concat(locale, "_provider"),
+            host: "616d6bdb6dacbb001794ca17.mockapi.io",
+            path: "/devnology/".concat(locale, "_provider"),
         };
     }
 }
--- src/api/options/index.ts
+++ src/api/options/index.ts
@@ -1,15 +1,6 @@
 function options(locale: string, id?: string) {
   if (id) {
     return {
-<<<<<<< HEAD
-      host: "json-mock-api.herokuapp.com",
-      path: `/${locale}_provider/${id}`,
-    };
-  } else {
-    return {
-      host: "json-mock-api.herokuapp.com",
-      path: `/${locale}_provider`,
-=======
       host: "616d6bdb6dacbb001794ca17.mockapi.io",
       path: `/devnology/${locale}_provider/${id}`,
     };
@@ -17,7 +8,6 @@ function options(locale: string, id?: string) {
     return {
       host: "616d6bdb6dacbb001794ca17.mockapi.io",
       path: `/devnology/${locale}_provider`,
->>>>>>> parent of 763bd24 (feat: change api option url)
     };
   }
 }
