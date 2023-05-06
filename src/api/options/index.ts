d12d0df97bf4a886e2fd64d2af233e0b1f20971d
cesarzxk <cesar3d.vargas@gmail.com>
2023-05-06 16:55:21 -0300

    Revert "feat: change api option url"
    This reverts commit 763bd242b77732a2c52be78bc2cd095eac66b6b2.

--- src/api/options/index.ts
+++ src/api/options/index.ts
@@ -1,6 +1,7 @@
 function options(locale: string, id?: string) {
   if (id) {
     return {
+<<<<<<< HEAD
       host: "json-mock-api.herokuapp.com",
       path: `/${locale}_provider/${id}`,
     };
@@ -8,6 +9,15 @@ function options(locale: string, id?: string) {
     return {
       host: "json-mock-api.herokuapp.com",
       path: `/${locale}_provider`,
+=======
+      host: "616d6bdb6dacbb001794ca17.mockapi.io",
+      path: `/devnology/${locale}_provider/${id}`,
+    };
+  } else {
+    return {
+      host: "616d6bdb6dacbb001794ca17.mockapi.io",
+      path: `/devnology/${locale}_provider`,
+>>>>>>> parent of 763bd24 (feat: change api option url)
     };
   }
 }
